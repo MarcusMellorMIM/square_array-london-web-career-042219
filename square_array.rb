7920e15181ef8f51fd07132f113434e0514f6e04
Marcus Mellor <marcus@mellor.com>
2019-03-17 17:34:06 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
   # your code here
+  #quitarray.each |array_value| do
+  #  new_array << array_value*array_value
+  #end
+  array.map!{|x|x*x}
+  new_array=array.each{|x|x*x}
+
 end
