f747c551601d41017acf512daedfc28517d03ff9
Marcus Mellor <marcus@mellor.com>
2019-03-17 17:33:23 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   # your code here
-  #array.each |array_value| do
+  #quitarray.each |array_value| do
   #  new_array << array_value*array_value
   #end
   array.map!{|x|x*x}
